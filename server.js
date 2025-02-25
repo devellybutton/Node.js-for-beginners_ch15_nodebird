c4b3a46de657fe3eb0e2e2c1f9da3ca04b7e7e3e
devellybutton <leegr0106@gmail.com>
2025-02-26 08:40:10 +0900

    server.js 수정

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
-app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번 포트에서 대기중");
+app.listen(8001, () => {
+  console.log(8001, "번 포트에서 대기중");
 });
